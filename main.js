d98279a193ee5b1d713f4a7df3af37351c03924a
arthur henrique.w.mello <106668417+art6399@users.noreply.github.com>
2024-04-16 11:55:50 -0300

    gg

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "Programação") {
+      alert("Bem vindo a minha pagina!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
-      alert("Não é isso! Tente novamente.");
+      alert("Não é essa senha.");
     }
   }
 }
